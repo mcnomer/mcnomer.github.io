c7d3186ff71e096b38e3da1117b9111d0bf2c7e1
Omer <57284289+mcnomer@users.noreply.github.com>
2025-04-23 21:12:33 +0100

    remade quickly using vite to inline a bunch of styles and stuff (also moved js down so it's no longer render-blocking)

--- gallery/scripts/createElement.mjs
+++
@@ -1,6 +0,0 @@
-export function createElement(type = "", className, innerHTML) {
-  const element = document.createElement(type);
-  if (className) element.className = className;
-  if (innerHTML) element.innerHTML = innerHTML;
-  return element;
-}
